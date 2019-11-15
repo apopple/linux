f888e5bb2d9558313f36bbc7cf12ca136239dd1d
Alistair Popple <alistair@popple.id.au>
2019-11-15 15:34:41 +1100

    Enable of platform device probing for PPC
    Signed-off-by: Alistair Popple <alistair@popple.id.au>

--- drivers/of/platform.c
+++ drivers/of/platform.c
@@ -509,7 +509,7 @@ int of_platform_default_populate(struct device_node *root,
 }
 EXPORT_SYMBOL_GPL(of_platform_default_populate);
 
-#ifndef CONFIG_PPC
+//#ifndef CONFIG_PPC
 static const struct of_device_id reserved_mem_matches[] = {
 	{ .compatible = "qcom,rmtfs-mem" },
 	{ .compatible = "qcom,cmd-db" },
@@ -544,7 +544,7 @@ static int __init of_platform_default_populate_init(void)
 	return 0;
 }
 arch_initcall_sync(of_platform_default_populate_init);
-#endif
+//#endif
 
 int of_platform_device_destroy(struct device *dev, void *data)
 {
